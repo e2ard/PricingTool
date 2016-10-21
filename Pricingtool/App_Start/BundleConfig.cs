600fa681deb2d92227b1e1acab02e73ea0481b59
Edvard <Edvard@E2RD>
2016-10-21 20:41:07 +0300

    no message

--- Pricingtool/App_Start/BundleConfig.cs
+++ Pricingtool/App_Start/BundleConfig.cs
@@ -20,15 +20,18 @@ namespace getLayout
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.min.js",
+                      "~/Scripts/moment.min.js",
+                      "~/Scripts/bootstrap-datetimepicker.min.js",
+                      "~/Scripts/bootstrap.min.js"));
+
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                    // "~/Content/bootstrap.min.css",
-                      "~/Content/Site.css",
-                       "~/Content/themes/ui-darkness/jquery.ui.all.css"));
+                      "~/Content/Site.css"/*,
+                       "~/Content/themes/ui-darkness/jquery.ui.all.css"*/));
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                "~/Content/bootstrap.min.css"));
         }
     }
 }
